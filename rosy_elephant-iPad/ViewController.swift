61987f0891a5b533a470b859d2f6df011e74f099
minhang.wang@icloud.com <minhang.wang@icloud.com>
2024-04-16 13:29:56 +0800

    Initial Commit

---
+++ rosy_elephant-iPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rosy_elephant-iPad
+//
+//  Created by wangminhang on 2024/4/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ rosy_elephant-iPadUITests/rosy_elephant_iPadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  rosy_elephant_iPadUITestsLaunchTests.swift
+//  rosy_elephant-iPadUITests
+//
+//  Created by wangminhang on 2024/4/16.
+//
+
+import XCTest
+
+final class rosy_elephant_iPadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
